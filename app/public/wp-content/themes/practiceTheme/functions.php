e95e3b57745d5f697a1f2aadd4694ccf78691818
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-09-08 10:59:26 -0500

    connect css for bootstrap, fontawesome and custom in functions.php

--- app/public/wp-content/themes/practiceTheme/functions.php
+++ app/public/wp-content/themes/practiceTheme/functions.php
@@ -1,7 +1,12 @@
 <?php 
 
 function luke_register_styles(){
-  wp_enqueue_style('luke-css', "/wp-content/themes/practiceTheme/themeTemplate/css/style.css", array(), '1.0', 'all');
+
+  $version = wp_get_theme()->get( 'Version' );
+  wp_enqueue_style('luke-style', "/wp-content/themes/practiceTheme/style.css", array('luke-bootstrap'), $version, 'all');
+  wp_enqueue_style('luke-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+  wp_enqueue_style('luke-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+
 }
 
 add_action( 'wp_enqueue_scripts', 'luke_register_styles');
