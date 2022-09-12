d79abcbfecaf796a894b0c1b875c5404a4579cf1
lolson5858 <109360413+lolson5858@users.noreply.github.com>
2022-09-12 13:13:06 -0500

    code for sidebar and footer widgets

--- app/public/wp-content/themes/practiceTheme/functions.php
+++ app/public/wp-content/themes/practiceTheme/functions.php
@@ -45,5 +45,35 @@ function luke_register_scripts(){
 
 add_action( 'wp_enqueue_scripts', 'luke_register_scripts');
 
+function lukewidget_areas(){
+
+    register_sidebar(
+      array(
+        'before_title' => '',
+        'after_title' => '',
+        'before_widget' => '',
+        'after_widget' => '',
+        'name' => 'Sidebar Area',
+        'id'  => 'sidebar-1',
+        'description' => 'Sidebar Widget'
+
+      )
+      );
+      register_sidebar(
+        array(
+          'before_title' => '',
+          'after_title' => '',
+          'before_widget' => '',
+          'after_widget' => '',
+          'name' => 'Footer Area',
+          'id'  => 'footer-1',
+          'description' => 'Footer Widget'
+  
+        )
+        );
+
+}
+
+add_action( 'widgets_init', 'lukewidget_areas');
 
 ?>
